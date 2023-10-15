fed9b493c70e9f73164dcebacf2d9be9f81b30c5
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-10-15 16:22:32 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(
   cors({
     origin: "https://login-register-technical-question-sooty.vercel.app",
-    methods: ["POST", "GET"],
     credentials: true,
   })
 );
