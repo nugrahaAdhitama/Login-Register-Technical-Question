3f1929126a02a522155b16c6704779cf5c48fb27
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-10-15 17:35:41 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,12 @@ app.use((req, res, next) => {
   next();
 });
 
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.get("/", (req, res) => {
   res.json("Hello World");
 });
